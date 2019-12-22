d25ffd9eff03d7b1603158324335058cc057ca32
Andrew Bailey <me@andrewbailey.dev>
2019-12-22 16:53:24 -0500

    Respond to media button intents

--- encore-player/src/main/java/dev/andrewbailey/encore/player/MediaPlayerService.kt
+++ encore-player/src/main/java/dev/andrewbailey/encore/player/MediaPlayerService.kt
@@ -2,7 +2,9 @@ package dev.andrewbailey.encore.player
 
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import androidx.media.session.MediaButtonReceiver
 import dev.andrewbailey.encore.player.notification.NotificationProvider
 import dev.andrewbailey.encore.player.notification.PlaybackNotifier
 import dev.andrewbailey.encore.player.os.MediaSessionController
@@ -50,6 +52,19 @@ abstract class MediaPlayerService(
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.hasExtra(Intent.EXTRA_KEY_EVENT) == true) {
+            if (Build.VERSION.SDK_INT >= 26) {
+                /*
+                 * We need to make sure we call startForeground since MediaButtonReceiver always
+                 * calls startForegroundService. Otherwise our service might get killed if the
+                 * service is in the background when it receives the command and does not end up
+                 * in a state that does not normally require a foreground notification.
+                 */
+                notifier.showNotification(foreground = true, playbackState = mediaPlayer.getState())
+            }
+            MediaButtonReceiver.handleIntent(mediaSessionController.mediaSession, intent)
+        }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
