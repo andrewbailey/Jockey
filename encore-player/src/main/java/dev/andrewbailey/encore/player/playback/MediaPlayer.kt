4b91fb7b0bf8c281612315cab2b7e2aef718cac8
Andrew Bailey <me@andrewbailey.dev>
2019-12-21 23:49:49 -0500

    Implement MediaPlayer's state

--- encore-player/src/main/java/dev/andrewbailey/encore/player/playback/MediaPlayer.kt
+++ encore-player/src/main/java/dev/andrewbailey/encore/player/playback/MediaPlayer.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.metadata.flac.PictureFrame
 import com.google.android.exoplayer2.metadata.id3.ApicFrame
 import dev.andrewbailey.encore.player.BuildConfig
-import dev.andrewbailey.encore.player.state.PlaybackState
+import dev.andrewbailey.encore.player.state.*
+import dev.andrewbailey.encore.player.state.SeekPosition.AbsoluteSeekPosition
+import dev.andrewbailey.encore.player.state.SeekPosition.ComputedSeekPosition
+import dev.andrewbailey.encore.player.state.diff.*
 import dev.andrewbailey.encore.player.util.getEntries
 import dev.andrewbailey.encore.player.util.getFormats
 import dev.andrewbailey.encore.player.util.toList
@@ -22,16 +26,93 @@ internal class MediaPlayer(
     private val exoPlayer = ExoPlayerFactory.newSimpleInstance(context)
     private val queue = MediaQueue(context, userAgent)
 
+    private val stateLock = Any()
+    private val differ = PlaybackStateDiffer()
+    private var lastDispatchedState: PlaybackState? = null
+    private var shouldDispatchStateChanges: Boolean = true
+
     init {
         extensions.forEach { it.initialize(this) }
     }
 
     fun getState(): PlaybackState {
-        TODO()
+        return synchronized(stateLock) {
+            val queue = queue.queueItems
+
+            PlaybackState(
+                transportState = if (queue.isEmpty()) {
+                    Idle
+                } else {
+                    Active(
+                        status = when {
+                            exoPlayer.isPlaying -> Status.PLAYING
+                            exoPlayer.playbackState == Player.STATE_ENDED -> Status.REACHED_END
+                            else -> Status.PAUSED
+                        },
+                        seekPosition = if (exoPlayer.isPlaying) {
+                            ComputedSeekPosition(exoPlayer.currentPosition)
+                        } else {
+                            AbsoluteSeekPosition(exoPlayer.currentPosition)
+                        },
+                        queue = queue,
+                        queueIndex = exoPlayer.currentWindowIndex
+                    )
+                },
+                repeatMode = when (val repeatMode = exoPlayer.repeatMode) {
+                    Player.REPEAT_MODE_OFF -> RepeatMode.REPEAT_NONE
+                    Player.REPEAT_MODE_ONE -> RepeatMode.REPEAT_ONE
+                    Player.REPEAT_MODE_ALL -> RepeatMode.REPEAT_ALL
+                    else -> {
+                        throw IllegalStateException("Unknown ExoPlayer repeat mode: $repeatMode")
+                    }
+                },
+                shuffleMode = if (exoPlayer.shuffleModeEnabled) {
+                    ShuffleMode.LINEAR
+                } else {
+                    ShuffleMode.SHUFFLED
+                }
+            )
+        }
     }
 
     fun setState(state: PlaybackState) {
-        TODO()
+        synchronized(stateLock) {
+            shouldDispatchStateChanges = false
+
+            val diff = differ.generateDiff(getState(), state)
+
+            for (operation in diff.operations) {
+                when (operation) {
+                    is TimelinePositionChange -> {
+                        exoPlayer.seekTo(operation.queueIndex, operation.seekPositionMillis)
+                    }
+                    is SetPlaying -> {
+                        exoPlayer.playWhenReady = operation.isPlaying
+                    }
+                    is StopPlayback -> {
+                        exoPlayer.stop(true)
+                    }
+                    is QueueModification -> {
+                        queue.changeQueue(operation.newQueue)
+                        exoPlayer.prepare(queue.mediaSource)
+                    }
+                }.let { /* Require when to be exhaustive */ }
+            }
+
+            shouldDispatchStateChanges = true
+            dispatchStateChange()
+        }
+    }
+
+    private fun dispatchStateChange() {
+        if (shouldDispatchStateChanges) {
+            val state = getState()
+            if (state != lastDispatchedState) {
+                lastDispatchedState = state
+                extensions.forEach { it.onNewPlayerState(state) }
+                observers.forEach { it.onPlaybackStateChanged(state) }
+            }
+        }
     }
 
     fun getArtwork(): Bitmap? {
