d0b78836411f690edf2a405731666d758688097e
Andrew Bailey <me@andrewbailey.dev>
2020-01-28 22:53:48 -0500

    Set up nav graph

--- app/src/main/java/dev/andrewbailey/music/ui/MainActivity.kt
+++ app/src/main/java/dev/andrewbailey/music/ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package dev.andrewbailey.music.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.andrewbailey.music.R
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
 }
