71559aff2cb1fa2432d56d653af5a0ed117baf9b
Andrew Bailey <me@andrewbailey.dev>
2020-05-03 14:45:32 -0400

    Remove Dagger workaround for kapt with Compose
    The latest version of Compose is playing nicer with annotation
    processors, so the workaround of pulling Dagger code out of files that
    contain `@Composable` functions is no longer necessary for the project
    to compile successfully.

--- app/src/main/java/dev/andrewbailey/music/ui/library/LibraryFragment.kt
+++ app/src/main/java/dev/andrewbailey/music/ui/library/LibraryFragment.kt
@@ -1,16 +1,34 @@
 package dev.andrewbailey.music.ui.library
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.compose.Composable
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.ui.core.Modifier
+import androidx.ui.foundation.Text
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import androidx.ui.material.*
+import androidx.ui.res.stringResource
+import dev.andrewbailey.encore.model.MediaAuthor
+import dev.andrewbailey.encore.model.MediaCollection
+import dev.andrewbailey.encore.model.MediaItem
+import dev.andrewbailey.encore.model.QueueItem
+import dev.andrewbailey.encore.player.controller.EncoreController
+import dev.andrewbailey.encore.player.state.*
 import dev.andrewbailey.music.JockeyApplication
+import dev.andrewbailey.music.R
+import dev.andrewbailey.music.ui.ComposableFragment
+import dev.andrewbailey.music.ui.player.CollapsedPlayerControls
+import dev.andrewbailey.music.util.fromRes
+import dev.andrewbailey.music.util.observe
+import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class LibraryFragment : Fragment() {
+class LibraryFragment : ComposableFragment() {
 
     @Inject
     lateinit var viewModelProvider: ViewModelProvider.Factory
@@ -22,16 +40,80 @@ class LibraryFragment : Fragment() {
         JockeyApplication.getComponent(requireContext()).inject(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return LibraryContentView(
-            context = requireContext(),
-            songs = viewModel.songs,
-            mediaController = viewModel.mediaController
-        )
+    @Composable
+    override fun onCompose() {
+        val playbackState = viewModel.mediaController.observeState(
+            seekUpdateFrequency = EncoreController.SeekUpdateFrequency.WhilePlayingEvery(
+                100,
+                TimeUnit.MILLISECONDS
+            )
+        ).asLiveData()
+
+        MaterialTheme(colorPalette()) {
+            Surface {
+                Column {
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.app_name)) }
+                    )
+
+                    Surface(Modifier.weight(1f)) {
+                        val library = observe(viewModel.songs)
+                        SongList(library.orEmpty().sortedBy { it.name }, viewModel.mediaController)
+                    }
+
+                    CollapsedPlayerControls(
+                        encoreController = viewModel.mediaController,
+                        playbackState = observe(playbackState) as? MediaPlayerState.Prepared
+                    )
+                }
+            }
+        }
     }
+
+    @Composable
+    private fun SongList(
+        songs: List<MediaItem>,
+        mediaController: EncoreController
+    ) {
+        VerticalScroller {
+            Column {
+                songs.forEachIndexed { index, song ->
+                    ListItem(
+                        text = song.name,
+                        secondaryText = formattedAlbumArtist(song.collection, song.author),
+                        onClick = {
+                            mediaController.setState(
+                                TransportState.Active(
+                                    status = PlaybackState.PLAYING,
+                                    seekPosition = SeekPosition.AbsoluteSeekPosition(0),
+                                    queue = QueueState.Linear(
+                                        queue = songs.map {
+                                            QueueItem(
+                                                queueId = UUID.randomUUID(),
+                                                mediaItem = it
+                                            )
+                                        },
+                                        queueIndex = index
+                                    ),
+                                    repeatMode = RepeatMode.REPEAT_NONE
+                                )
+                            )
+                        }
+                    )
+                    Divider()
+                }
+            }
+        }
+    }
+
+    private fun formattedAlbumArtist(album: MediaCollection?, artist: MediaAuthor?): String =
+        listOfNotNull(album?.name, artist?.name).joinToString(" - ")
+
+    private fun colorPalette() = lightColorPalette(
+        primary = Color.fromRes(requireContext(), R.color.colorPrimary),
+        primaryVariant = Color.fromRes(requireContext(), R.color.colorPrimary),
+        secondary = Color.fromRes(requireContext(), R.color.colorAccent),
+        secondaryVariant = Color.fromRes(requireContext(), R.color.colorAccentDark)
+    )
 
 }
