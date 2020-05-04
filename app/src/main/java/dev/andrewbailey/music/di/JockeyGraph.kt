e2619f393c10fd6945f8d6dd55e700f9d2e76b72
Andrew Bailey <me@andrewbailey.dev>
2020-05-03 21:00:56 -0400

    Add All Songs section to browser implementation

--- app/src/main/java/dev/andrewbailey/music/di/JockeyGraph.kt
+++ app/src/main/java/dev/andrewbailey/music/di/JockeyGraph.kt
@@ -1,8 +1,11 @@
 package dev.andrewbailey.music.di
 
+import dev.andrewbailey.music.player.PlayerService
 import dev.andrewbailey.music.ui.library.LibraryFragment
 
 interface JockeyGraph {
+
+    fun inject(service: PlayerService)
 
     fun inject(fragment: LibraryFragment)
 
--- app/src/main/java/dev/andrewbailey/music/player/PlayerService.kt
+++ app/src/main/java/dev/andrewbailey/music/player/PlayerService.kt
@@ -1,7 +1,12 @@
 package dev.andrewbailey.music.player
 
 import dev.andrewbailey.encore.player.MediaPlayerService
+import dev.andrewbailey.encore.player.browse.BrowserDirectory
 import dev.andrewbailey.encore.player.browse.BrowserHierarchy
+import dev.andrewbailey.encore.provider.MediaProvider
+import dev.andrewbailey.music.JockeyApplication
+import dev.andrewbailey.music.R
+import javax.inject.Inject
 
 class PlayerService : MediaPlayerService(
     tag = "Jockey",
@@ -9,8 +14,21 @@ class PlayerService : MediaPlayerService(
     notificationProvider = PlayerNotifier()
 ) {
 
+    @Inject lateinit var mediaProvider: MediaProvider
+
+    override fun onCreate() {
+        JockeyApplication.getComponent(this).inject(this)
+        super.onCreate()
+    }
+
     override fun onCreateMediaBrowserHierarchy(): BrowserHierarchy {
         return BrowserHierarchy {
+            staticPath(BrowserDirectory.BrowserPath(
+                id = "songs",
+                name = getString(R.string.media_browser_all_songs_section)
+            )) {
+                mediaItems("song") { mediaProvider.getAllMedia() }
+            }
         }
     }
 
