2e6ccabdbea866c211818404192ba2fabd8dbb59
Andrew Bailey <me@andrewbailey.dev>
2021-01-15 22:25:58 -0500

    Update Compose to 1.0.0-alpha10

--- app/src/main/java/dev/andrewbailey/music/ui/library/common/SongList.kt
+++ app/src/main/java/dev/andrewbailey/music/ui/library/common/SongList.kt
@@ -1,7 +1,7 @@
 package dev.andrewbailey.music.ui.library.common
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.lazy.LazyColumnForIndexed
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.ListItem
 import androidx.compose.material.Text
@@ -17,30 +17,34 @@ fun SongList(
     modifier: Modifier = Modifier,
     onClickSong: ((index: Int, song: MediaStoreSong) -> Unit)? = null
 ) {
-    LazyColumnForIndexed(
-        items = songs,
+    LazyColumn(
         modifier = modifier
-    ) { index, song ->
-        ListItem(
-            text = {
-                Text(
-                    text = song.name,
-                    maxLines = 1
+    ) {
+        itemsIndexed(
+            items = songs,
+            itemContent = { index, song ->
+                ListItem(
+                    text = {
+                        Text(
+                            text = song.name,
+                            maxLines = 1
+                        )
+                    },
+                    secondaryText = {
+                        Text(
+                            text = formattedAlbumArtist(song.album, song.artist),
+                            maxLines = 1
+                        )
+                    },
+                    modifier = if (onClickSong != null) {
+                        Modifier.clickable(onClick = { onClickSong(index, song) })
+                    } else {
+                        Modifier
+                    }
                 )
-            },
-            secondaryText = {
-                Text(
-                    text = formattedAlbumArtist(song.album, song.artist),
-                    maxLines = 1
-                )
-            },
-            modifier = if (onClickSong != null) {
-                Modifier.clickable(onClick = { onClickSong(index, song) })
-            } else {
-                Modifier
+                Divider()
             }
         )
-        Divider()
     }
 }
 
