a358cf2d0f0de4750fe8acbbe97d0d83e1ac60c3
Andrew Bailey <me@andrewbailey.dev>
2021-12-24 16:24:47 -0500

    Refactor Now Playing UI elements

---
+++ jockey/app/src/main/java/dev/andrewbailey/music/ui/player/NowPlayingControls.kt
@@ -0,0 +1,256 @@
+package dev.andrewbailey.music.ui.player
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.andrewbailey.encore.player.state.MediaPlayerState
+import dev.andrewbailey.encore.player.state.PlaybackState
+import dev.andrewbailey.music.R
+import dev.andrewbailey.music.model.Song
+import dev.andrewbailey.music.ui.data.LocalPlaybackController
+
+@Composable
+fun NowPlayingControls(
+    playbackState: MediaPlayerState<Song>?,
+    modifier: Modifier = Modifier
+) {
+    if (playbackState is MediaPlayerState.Prepared) {
+        val playbackController = LocalPlaybackController.current
+        val transportState = playbackState.transportState
+        val nowPlaying = transportState.queue.nowPlaying.mediaItem
+        NowPlayingControls(
+            songTitle = nowPlaying.name,
+            albumName = nowPlaying.album?.name ?: stringResource(R.string.unknown_album),
+            artistName = nowPlaying.artist?.name ?: stringResource(R.string.unknown_artist),
+            songDurationMs = playbackState.durationMs ?: 0,
+            seekPositionMs = transportState.seekPosition.seekPositionMillis,
+            isPlaying = transportState.status == PlaybackState.PLAYING,
+            onSkipNext = playbackController::skipNext,
+            onSkipPrevious = playbackController::skipPrevious,
+            onPlay = playbackController::play,
+            onPause = playbackController::pause,
+            onSeekToPositionMs = playbackController::seekTo,
+            modifier = modifier
+        )
+    } else {
+        NowPlayingControls(
+            songTitle = stringResource(R.string.nothing_playing),
+            albumName = "",
+            artistName = "",
+            songDurationMs = 1L,
+            seekPositionMs = 0L,
+            isPlaying = false,
+            enabled = false,
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+private fun NowPlayingControls(
+    songTitle: String,
+    albumName: String,
+    artistName: String,
+    songDurationMs: Long,
+    seekPositionMs: Long,
+    isPlaying: Boolean,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onSkipNext: () -> Unit = {},
+    onSkipPrevious: () -> Unit = {},
+    onPlay: () -> Unit = {},
+    onPause: () -> Unit = {},
+    onSeekToPositionMs: (Long) -> Unit = {}
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        SongMetadata(
+            songTitle = songTitle,
+            albumName = albumName,
+            artistName = artistName,
+            enabled = enabled
+        )
+
+        SeekSlider(
+            songDurationMs = songDurationMs,
+            seekPositionMs = seekPositionMs,
+            onSeekToPositionMs = onSeekToPositionMs,
+            enabled = enabled,
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = 4.dp)
+                .height(36.dp)
+        )
+
+        TransportControls(
+            isPlaying = isPlaying,
+            onSkipNext = onSkipNext,
+            onSkipPrevious = onSkipPrevious,
+            onPlay = onPlay,
+            onPause = onPause,
+            enabled = enabled,
+            modifier = Modifier.fillMaxWidth(0.85f)
+        )
+    }
+}
+
+@Composable
+private fun SongMetadata(
+    songTitle: String,
+    albumName: String,
+    artistName: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    val textColor = MaterialTheme.colors.onBackground.copy(
+        alpha = if (enabled) 1f else 0.38f
+    )
+
+    Column(modifier = modifier) {
+        Text(
+            text = songTitle,
+            style = MaterialTheme.typography.body1,
+            color = textColor,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = albumName,
+            style = MaterialTheme.typography.body2,
+            color = textColor,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Text(
+            text = artistName,
+            style = MaterialTheme.typography.body2,
+            color = textColor,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun SeekSlider(
+    songDurationMs: Long,
+    seekPositionMs: Long,
+    onSeekToPositionMs: (Long) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    val userSeekPosition = remember { mutableStateOf<Float?>(null) }
+    val sliderInteractionState = remember { MutableInteractionSource() }
+
+    Slider(
+        valueRange = 0f..songDurationMs.toFloat(),
+        interactionSource = sliderInteractionState,
+        value = userSeekPosition.value
+            .takeIf { sliderInteractionState.collectIsDraggedAsState().value }
+            ?: seekPositionMs.toFloat(),
+        onValueChange = { userSeekPosition.value = it },
+        onValueChangeFinished = {
+            onSeekToPositionMs(
+                checkNotNull(userSeekPosition.value) {
+                    "Failed to finalize seek because the requested seek position is unknown."
+                }.toLong()
+            )
+            userSeekPosition.value = null
+        },
+        enabled = enabled,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun TransportControls(
+    isPlaying: Boolean,
+    onSkipNext: () -> Unit,
+    onSkipPrevious: () -> Unit,
+    onPlay: () -> Unit,
+    onPause: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        TransportButton(
+            painter = painterResource(R.drawable.ic_skip_previous),
+            label = stringResource(R.string.content_description_skip_previous),
+            onClick = onSkipPrevious,
+            enabled = enabled,
+            modifier = Modifier.weight(1.0f)
+        )
+
+        TransportButton(
+            painter = painterResource(
+                id = if (isPlaying) R.drawable.ic_pause else R.drawable.ic_play
+            ),
+            label = stringResource(
+                id = if (isPlaying) {
+                    R.string.content_description_pause
+                } else {
+                    R.string.content_description_play
+                }
+            ),
+            onClick = if (isPlaying) { onPause } else { onPlay },
+            enabled = enabled,
+            modifier = Modifier.weight(1.0f)
+        )
+        TransportButton(
+            painter = painterResource(R.drawable.ic_skip_next),
+            label = stringResource(R.string.content_description_skip_next),
+            onClick = onSkipNext,
+            enabled = enabled,
+            modifier = Modifier.weight(1.0f)
+        )
+    }
+}
+
+@Composable
+private fun TransportButton(
+    painter: Painter,
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        IconButton(
+            onClick = onClick,
+            enabled = enabled
+        ) {
+            Icon(
+                painter = painter,
+                contentDescription = label,
+                tint = MaterialTheme.colors.onBackground
+            )
+        }
+    }
+}
--- jockey/app/src/main/java/dev/andrewbailey/music/ui/player/NowPlayingRoot.kt
+++ jockey/app/src/main/java/dev/andrewbailey/music/ui/player/NowPlayingRoot.kt
@@ -4,18 +4,13 @@ import androidx.annotation.FloatRange
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsDraggedAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
@@ -27,15 +22,11 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.ListItem
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.drawWithContent
@@ -54,7 +45,6 @@ import com.google.accompanist.insets.rememberInsetsPaddingValues
 import com.google.accompanist.insets.statusBarsPadding
 import dev.andrewbailey.encore.model.QueueItem
 import dev.andrewbailey.encore.player.state.MediaPlayerState
-import dev.andrewbailey.encore.player.state.PlaybackState
 import dev.andrewbailey.encore.player.state.ShuffleMode
 import dev.andrewbailey.encore.player.state.TransportState
 import dev.andrewbailey.music.R
@@ -175,152 +165,6 @@ private fun NowPlayingToolbar(
 
         }
     )
-}
-
-@Composable
-private fun NowPlayingControls(
-    playbackState: MediaPlayerState<Song>?,
-    modifier: Modifier = Modifier
-) {
-    if (playbackState is MediaPlayerState.Prepared) {
-        ActiveNowPlayingControls(
-            playbackState = playbackState,
-            modifier = modifier
-        )
-    } else {
-        InactiveNowPlayingControls(
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-private fun ActiveNowPlayingControls(
-    playbackState: MediaPlayerState.Prepared<Song>,
-    modifier: Modifier
-) {
-    val playbackController = LocalPlaybackController.current
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier
-    ) {
-        val userSeekPosition = remember { mutableStateOf<Float?>(null) }
-        val nowPlaying = playbackState.transportState.queue.nowPlaying.mediaItem
-
-        Text(
-            text = nowPlaying.name,
-            style = MaterialTheme.typography.body1,
-            color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Text(
-            text = nowPlaying.album?.name ?: "No album",
-            style = MaterialTheme.typography.body2,
-            color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Text(
-            text = nowPlaying.artist?.name ?: "No artist",
-            style = MaterialTheme.typography.body2,
-            color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        val sliderInteractionState = remember { MutableInteractionSource() }
-        Slider(
-            valueRange = 0f..(playbackState.durationMs?.toFloat() ?: 0f),
-            interactionSource = sliderInteractionState,
-            value = userSeekPosition.value
-                .takeIf { sliderInteractionState.collectIsDraggedAsState().value }
-                ?: playbackState.transportState.seekPosition.seekPositionMillis.toFloat(),
-            onValueChange = { userSeekPosition.value = it },
-            onValueChangeFinished = {
-                playbackController.seekTo(
-                    checkNotNull(userSeekPosition.value) {
-                        "Failed to finalize seek because the requested seek position is unknown."
-                    }.toLong()
-                )
-                userSeekPosition.value = null
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .offset(y = 4.dp)
-                .height(36.dp)
-        )
-
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth(0.85f)
-        ) {
-            Box(
-                modifier = Modifier.weight(1.0f),
-                contentAlignment = Alignment.Center
-            ) {
-                IconButton(onClick = playbackController::skipPrevious) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_skip_previous),
-                        contentDescription = stringResource(
-                            id = R.string.content_description_skip_previous
-                        ),
-                        tint = MaterialTheme.colors.onBackground
-                    )
-                }
-            }
-
-            Box(
-                modifier = Modifier.weight(1.0f),
-                contentAlignment = Alignment.Center
-            ) {
-                if (playbackState.transportState.status == PlaybackState.PLAYING) {
-                    IconButton(onClick = playbackController::pause) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_pause),
-                            contentDescription = stringResource(R.string.content_description_pause),
-                            tint = MaterialTheme.colors.onBackground
-                        )
-                    }
-                } else {
-                    IconButton(onClick = playbackController::play) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_play),
-                            contentDescription = stringResource(R.string.content_description_play),
-                            tint = MaterialTheme.colors.onBackground
-                        )
-                    }
-                }
-            }
-
-            Box(
-                modifier = Modifier.weight(1.0f),
-                contentAlignment = Alignment.Center
-            ) {
-                IconButton(onClick = playbackController::skipNext) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_skip_next),
-                        contentDescription = stringResource(R.string.content_description_skip_next),
-                        tint = MaterialTheme.colors.onBackground
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun InactiveNowPlayingControls(
-    modifier: Modifier = Modifier
-) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = "Nothing is playing"
-        )
-    }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
